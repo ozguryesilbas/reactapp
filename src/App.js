d9d78b32893adb48d6922dad381e4402e8ccf464
ozguryesilbas@live.com <ozgur1586>
2020-03-22 14:32:08 +0300

    ilk denemeler

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    render() {
+        const x = "ali";
+        const isAuth = true;
+        return (
+            /*her render fonksiyonu bir component döner*/
+            <div className="App">
+                <h1>Merhaba React</h1>
+                <h1>asdasdsda</h1>
+
+                {/*jsxde class for gibi bazı kullanımlar yapılamaz, bunun yerine classname kullanılır*/}
+                <h1 className="baslik">asdasdsda</h1>
+
+                1+1
+                {/* 1+1 yazıldığında yazı olarak algılanır, jsx içinde javascript kullanmak için
+                 {} parantezleri kullanılır*/}
+
+                <h1>{"Mehmet".toUpperCase()}</h1>
+                <h1>{4 + 3}</h1>
+                <h1>{x}</h1>
+
+                <div>
+                    {
+                        isAuth ? <p>Kullanıcı kayıtlı</p> : null
+                    }
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
