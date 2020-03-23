e3d98313c10d383d3db5a3b0b06f03a32638a1b5
ozguryesilbas@live.com <ozgur1586>
2020-03-23 15:48:09 +0300

    state eklendi

--- src/components/Personel.js
+++ src/components/Personel.js
@@ -1,10 +1,29 @@
 import React, {Component} from "react";
 
 class Personel extends Component {
+
+    /* state bir componentin o anki durumunu tutan ve değişik propertylerden olusan bir javascript objesidir
+    state değiştiğinde sanal dom bunu algılar ve sadece sayfada stati değişen yer güncellenir
+    stateleri propslar yardımıyla aktarabilriz
+    2 türlü tanımlayabiliriz clasın hemen içinde veya constructorda */
+
+    /*    state = {
+        isVisible : true
+    }*/
+
+    constructor(props){
+        super(props);
+        this.state = {
+            test : "Test",
+            isVisible : true
+        }
+    }
+
     render() {
 
         //javascript destructing özelliği ile this.props şeklinde uzun kullanımdan kurtulabiliriz
         const {name,department,salary} = this.props;
+        const {isVisible} = this.state;
 
         return (
             <div className="col-md-8 mb-4">
@@ -13,10 +32,18 @@ class Personel extends Component {
                         <h4 className="d-inline">{name}</h4>
                         <i className="fas fa-address-book" style={{cursor:"pointer"}}></i>
                     </div>
-                    <div className="card-body">
-                        <p className="card-text">Bölüm : {department}</p>
-                        <p className="card-text">Maaş : {salary}</p>
-                    </div>
+
+                    {
+                        isVisible ?
+                            <div className="card-body">
+                                <p className="card-text">Bölüm : {department}</p>
+                                <p className="card-text">Maaş : {salary}</p>
+                                <p>{this.state.test}</p>
+                            </div>
+                            :
+                            null
+                    }
+
                 </div>
             </div>
         );
