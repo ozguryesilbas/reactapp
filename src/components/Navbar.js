aeb2a28ea5eb192626bfc11f92c4202987d25108
ozguryesilbas@live.com <ozgur1586>
2020-03-23 10:54:06 +0300

    arrow function gösterimi eklendi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-function Navbar() { /*eğer component bir state almıyorsa sabit olucaksa bunu functional component olarak da olusturabiliriz*/
+/*const Navbar = () => {*/    /* bu şekilde arrow function olarak da yazabiliriz*/
+function Navbar() {           /* eğer component bir state almıyorsa sabit olucaksa bunu functional component olarak da olusturabiliriz*/
     return (
         <div>
             <h3>User App</h3>
