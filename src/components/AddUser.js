3f1e0a513ac16bdfc20e8744dba4572c37f0ad09
ozguryesilbas@live.com <ozgur1586>
2020-03-25 00:55:53 +0300

    forma girilen değerler state'e bind edildi, value ve onchange kullanıldı

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,7 +1,23 @@
 import React, {Component} from "react";
 
 class AddUser extends Component {
+
+    state = {
+        name: "",
+        department: "",
+        salary: ""
+    }
+
+    /*içindeki değerlerin değişebilmesi için mutlaka onchange eveti olmalı*/
+    changeInput = (e) => {
+        this.setState({
+            //normalede name : e.target.value şeklinde yaptık, aşağıdaki kullanım ise tümü için geçerli oldu
+            [e.target.name] : e.target.value
+        })
+    }
+
     render() {
+        const {name, salary, department} = this.state;
 
         return (
             <div className="col-md-8 mb-4">
@@ -16,6 +32,9 @@ class AddUser extends Component {
                                 <input type="text"
                                        placeholder="Enter Name"
                                        name="name"
+                                       value={name}
+                                    /*içindeki değerlerin değişebilmesi için mutlaka onchange eveti olmalı*/
+                                       onChange={this.changeInput}
                                        className="form-control"
                                        id="id"></input>
                             </div>
@@ -25,6 +44,8 @@ class AddUser extends Component {
                                 <input type="text"
                                        placeholder="Enter Department"
                                        name="department"
+                                       value={department}
+                                       onChange={this.changeInput}
                                        className="form-control"
                                        id="id"></input>
                             </div>
@@ -34,6 +55,8 @@ class AddUser extends Component {
                                 <input type="text"
                                        placeholder="Enter Salary"
                                        name="salary"
+                                       value={salary}
+                                       onChange={this.changeInput}
                                        className="form-control"
                                        id="id"></input>
                             </div>
